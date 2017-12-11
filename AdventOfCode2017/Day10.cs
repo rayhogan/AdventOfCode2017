b05eef7c452bd13994ca49f64c9963c2b5247f29
Ray Hogan <rayhogan@gmail.com>
2017-12-11 21:47:09 +0000

    Completed day 10. Needed the tests for this one. Part II horribly worded.

--- AdventOfCode2017/Day10.cs
+++ AdventOfCode2017/Day10.cs
@@ -32,15 +32,9 @@ namespace AdventOfCode2017
 
             string[] split = input.Split(',');
 
-            foreach (int i in data)
-            {
-                Console.Write(i);
-            }
-            Console.WriteLine("");
-
             foreach (string s in split)
             {
-                
+
 
                 int processing = Int32.Parse(s);
 
@@ -53,9 +47,9 @@ namespace AdventOfCode2017
                     {
                         indexes.Add(pos);
 
-                        clipboard.Add(new Tuple<int,int>(pos, data[pos]));
+                        clipboard.Add(new Tuple<int, int>(pos, data[pos]));
 
-                        if (pos + 1 > data.Length-1)
+                        if (pos + 1 > data.Length - 1)
                             pos = 0;
                         else
                             pos++;
@@ -63,18 +57,18 @@ namespace AdventOfCode2017
 
 
                     // Swap them about now
-                    for(int i=0;i<indexes.Count;i++)
+                    for (int i = 0; i < indexes.Count; i++)
                     {
-                        data[indexes[(indexes.Count-1)-i]] = clipboard[i].Item2;
+                        data[indexes[(indexes.Count - 1) - i]] = clipboard[i].Item2;
                     }
-                    
-                   
+
+
                 }
 
                 position = position + processing + skipSize;
                 skipSize++;
 
-                while (position > data.Length)
+                while (position >= data.Length)
                 {
                     position = position - data.Length;
                 }
@@ -91,15 +85,129 @@ namespace AdventOfCode2017
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Part II");
             Console.ForegroundColor = ConsoleColor.White;
+
+            List<int> transformed = TransformToASCII(input);
+            //17, 31, 73, 47, 23
+            transformed.Add(17);
+            transformed.Add(31);
+            transformed.Add(73);
+            transformed.Add(47);
+            transformed.Add(23);
+
+            int skipSize = 0;
+            int position = 0;
+
+            List<Tuple<int, int>> clipboard = new List<Tuple<int, int>>();
+
+            int[] data = GetIntArray(256);
+
+            for (int iCount = 0; iCount < 64; iCount++)
+            {
+                foreach (int inty in transformed)
+                {
+                    int processing = inty;
+
+                    if (processing > 1)
+                    {
+                        List<int> indexes = new List<int>();
+
+                        int pos = position;
+                        for (int i = 0; i < processing; i++)
+                        {
+                            indexes.Add(pos);
+
+                            clipboard.Add(new Tuple<int, int>(pos, data[pos]));
+
+                            if (pos + 1 > data.Length - 1)
+                                pos = 0;
+                            else
+                                pos++;
+                        }
+
+
+                        // Swap them about now
+                        for (int i = 0; i < indexes.Count; i++)
+                        {
+                            data[indexes[(indexes.Count - 1) - i]] = clipboard[i].Item2;
+                        }
+
+
+                    }
+
+                    position = position + processing + skipSize;
+                    skipSize++;
+
+                    while (position >= data.Length)
+                    {
+                        position = position - data.Length;
+                    }
+
+
+                    clipboard.Clear();
+
+                }
+            }
+
+
+
+            List<int> xorResults = XOROperations(data);
+            Console.WriteLine("Hex: " + HexString(xorResults));
+
+
+
         }
 
         public static int[] GetIntArray(int size)
         {
             int[] output = new int[size];
-            for(int i=0;i<output.Length;i++)
+            for (int i = 0; i < output.Length; i++)
             {
                 output[i] = i;
             }
+            return output;
+        }
+
+        public static List<int> TransformToASCII(string input)
+        {
+            List<int> output = new List<int>();
+
+            char[] split = input.ToCharArray();
+            foreach (char c in split)
+            {
+                int unicode = c;
+                output.Add(unicode);
+            }
+
+            return output;
+        }
+
+        public static string HexString(List<int> input)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (int i in input)
+            {
+                if(i<10)                
+                    sb.Append(0 +""+ i);                
+                else
+                    sb.Append(i.ToString("X"));
+
+                Console.WriteLine("Value: " + i + " becomes " + i.ToString("X"));
+            }
+
+            return sb.ToString().ToLower();
+        }
+
+        public static List<int> XOROperations(int[] data)
+        {
+            List<int> output = new List<int>();
+
+            for (int i = 0; i < data.Length; i += 16)
+            {
+                output.Add(data[i] ^ data[i + 1] ^ data[i + 2] ^ data[i + 3] ^ data[i + 4] ^ data[i + 5] ^ data[i + 6] ^ data[i + 7] ^ data[i + 8] ^ data[i + 9] ^ data[i + 10] ^ data[i + 11] ^ data[i + 12] ^ data[i + 13] ^ data[i + 14] ^ data[i + 15]);
+
+            }
+
             return output;
         }
     }
--- AoC/Day10Tests.cs
+++ AoC/Day10Tests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AdventOfCode2017;
+using System.Collections.Generic;
 
 namespace AoC
 {
@@ -14,10 +15,90 @@ namespace AoC
 
             Assert.AreEqual(testArray.Length, 256);
 
-            for(int i=0;i<testArray.Length;i++)
+            for (int i = 0; i < testArray.Length; i++)
             {
                 Assert.AreEqual(testArray[i], i);
             }
         }
+
+        [TestMethod]
+        public void TransformToASCII()
+        {
+            List<int> output = new List<int>();
+            //49,44,50,44,51
+            output.Add(49);
+            output.Add(44);
+            output.Add(50);
+            output.Add(44);
+            output.Add(51);
+
+            List<int> result = Day10.TransformToASCII("1,2,3");
+
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                Assert.AreEqual(output[i], result[i]);
+            }
+
+        }
+
+        [TestMethod]
+        public void XORTests()
+        {
+            int[] input = new int[16];
+            //65 ^ 27 ^ 9 ^ 1 ^ 4 ^ 3 ^ 40 ^ 50 ^ 91 ^ 7 ^ 6 ^ 0 ^ 2 ^ 5 ^ 68 ^ 22
+            input[0] = 65;
+            input[1] = 27;
+            input[2] = 9;
+            input[3] = 1;
+            input[4] = 4;
+            input[5] = 3;
+            input[6] = 40;
+            input[7] = 50;
+            input[8] = 91;
+            input[9] = 7;
+            input[10] = 6;
+            input[11] = 0;
+            input[12] = 2;
+            input[13] = 5;
+            input[14] = 68;
+            input[15] = 22;
+
+            List<int> result = Day10.XOROperations(input);
+
+            Assert.AreEqual(result[0], 64);
+        }
+
+        [TestMethod]
+        public void End2End()
+        {
+            List<int> output = new List<int>();
+            //49,44,50,44,51
+            output.Add(49);
+            output.Add(44);
+            output.Add(50);
+            output.Add(44);
+            output.Add(51);
+
+            List<int> result = Day10.TransformToASCII("1,2,3");
+
+
+            
+        }
+
+        [TestMethod]
+        public void HexConversion()
+        {
+            //64, 7, 255
+            List<int> input = new List<int>();
+            input.Add(64);
+            input.Add(07);
+            input.Add(255);
+
+            string result = Day10.HexString(input);
+
+            Assert.AreEqual(result, "4007ff");
+        }
+
     }
 }
